f1ca44c796dd773d80518b775bdcd6e3f98f127c
ngduyanhece <ngduyanhece@gmail.com>
2024-11-13 16:33:06 +0700

    Add proper HTML5 doctype declaration Configure STATIC_URL and STATIC_ROOT include a descriptive docstring

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'restaurant'
 ]
 
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
@@ -118,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
--- restaurant/views.py
+++ restaurant/views.py
@@ -3,4 +3,11 @@ from django.shortcuts import render
 
 
 def index(request):
+    """Render the main index page of the Little Lemon restaurant.
+
+    Args:
+        request: HttpRequest object
+    Returns:
+        Rendered index.html template
+    """
     return render(request, 'index.html', {})
